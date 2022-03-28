6274462c3fa87e2079fb1593d0430df55fa7c9f7
yuki <tada950918@gmail.com>
2022-03-28 09:22:20 +0900

    イメージの初期設定

--- routes/owner.php
+++ routes/owner.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,6 +36,9 @@ Route::prefix('shops')->middleware('auth:owners')->group(function() {
     Route::post( 'update/{shop}' , [ShopController::class , 'update'] )
         ->name("shops.update");
 });
+
+Route::resource("images",ImageController::class)
+    ->middleware('auth:owners')->except(["show"]);
 
 // dashboardにアクセスしたときに、オーナーの権限を持っている場合に、dashboardを返す
 // 以下全てのauthで「:owners」を追加して、guard機能の設定を行う
