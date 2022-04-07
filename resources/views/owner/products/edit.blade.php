282af3aadce01d413ca53282dc736fe4d5c76203
yuki <tada950918@gmail.com>
2022-04-07 09:04:18 +0900

    product delete

--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -99,6 +99,13 @@
                           </div>
                       </div>
                   </form>
+                  <form id="delete_{{$product->id}}" method="post" action="{{ route( 'owner.products.destroy' , ['product' => $product->id] ) }}">
+                      @csrf
+                      @method("delete")
+                      <div class="flex justify-center p-2 w-full">
+                          <a data-id="{{$product->id}}" onclick="deletePost(this)" class="text-white bg-red-500 border-0 py-2 px-4 focus:outline-none hover:bg-red-400 rounded">削除</a>
+                      </div>
+                  </form>
               </div>
           </div>
       </div>
@@ -119,5 +126,11 @@
         MicroModal.close(modal); //モーダルを閉じる
       })
     })
+    function deletePost(e) {
+      'use strict';
+      if (confirm('本当に削除してもいいですか?')) {
+        document.getElementById('delete_' + e.dataset.id).submit();
+      }
+    }
   </script>
 </x-app-layout>
