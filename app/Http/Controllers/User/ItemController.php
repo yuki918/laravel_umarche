ab3fbf6966d86a02408620fc2b5f9ed5deada64a
yuki <tada950918@gmail.com>
2022-04-09 15:42:48 +0900

    query 作成

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
@@ -10,7 +11,26 @@ class ItemController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
+        $stocks = DB::table('t_stocks')
+            ->select('product_id',DB::raw('sum(quantity) as quantity'))
+            ->groupBy('product_id')
+            ->having('quantity','>=',1);
+        $products = DB::table('products')
+            ->joinSub($stocks,'stock',function($join) {
+                $join->on('products.id','=','stock.product_id');
+            })
+            ->join('shops','products.shop_id','=','shops.id')
+            ->join('secondary_categories','products.secondary_category_id', '=','secondary_categories.id')
+            ->join('images as image01', 'products.image01', '=', 'image01.id')
+            ->where('shops.is_selling',true)
+            ->where('products.is_selling',true)
+            ->select('products.id as id', 'products.name as name', 'products.price'
+                    ,'products.sort_order as sort_order'
+                    ,'products.information', 'secondary_categories.name as category'
+                    ,'image01.filename as filename')
+            ->get();
+
+        // dd($stocks,$products);
         return view('user.index',compact('products'));
     }
 }
