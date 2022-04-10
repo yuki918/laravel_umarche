a3b591280a08a4f878250d9020f6c3834d1a9f92
yuki <tada950918@gmail.com>
2022-04-10 18:07:48 +0900

    stripeのコントローラー側の設定

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -51,4 +51,41 @@ class CartController extends Controller
         Cart::where('product_id',$id)->where('user_id',Auth::id())->delete();
         return redirect()->route('user.cart.index');
     }
+
+    public function checkout()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+
+        // stripeに情報を渡すために、stripe用の命名規則で情報を渡す
+        // https://stripe.com/docs/api/checkout/sessions/create
+        $lineItems = [];
+        foreach($products as $product) {
+            $lineItem = [
+                'name'        => $product->name,
+                'description' => $product->information,
+                'amount'      => $product->price,
+                'currency'    => 'jpy',
+                'quantity'    => $product->pivot->quantity,
+            ];
+            array_push($lineItems,$lineItem);
+        }
+        // dd($lineItems);
+
+        // stripe側に秘密鍵と商品情報を渡す
+        // 秘密鍵はenvファイルに記述しているので、変数として取得する
+        // https://stripe.com/docs/checkout/quickstart
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+        $checkout_session = \Stripe\Checkout\Session::create([
+            'line_items'  => [$lineItems],
+            'mode'        => 'payment',
+            // 支払いが成功した場合のリダイレクト処理
+            'success_url' => route('user.items.index'),
+            // 支払いがキャンセルした場合のリダイレクト処理
+            'cancel_url'  => route('user.cart.index'),
+        ]);
+        // 公開鍵の取得。秘密鍵と公開鍵の両方の情報を渡すことで、決済することができる
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+        return view('user.checkout',compact('session','publicKey'));
+    }
 }
